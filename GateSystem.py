bc133574fa5c426635126f3fee80f5b181ee762e
keerati2106 <keerati2106@gmail.com>
2021-05-02 14:13:28 +0700

    Update GateSystem.py

--- GateSystem.py
+++ GateSystem.py
@@ -9,26 +9,6 @@
 #    MFRC522-Python is a simple Python implementation for
 #    the MFRC522 NFC Card Reader for the Raspberry Pi.
 #
-#    DoorSytem is an implementation of MFRC-Python modified
-#    to use spidev instead of spi
-#    to be able to use python3
-#
-#    MFRC522-Python is free software:
-#    you can redistribute it and/or modify
-#    it under the terms of
-#    the GNU Lesser General Public License as published by the
-#    Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    MFRC522-Python is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the
-#    GNU Lesser General Public License along with MFRC522-Python.
-#    If not, see <http://www.gnu.org/licenses/>.
-#
 
 import RPi.GPIO as GPIO
 import MFRC522
